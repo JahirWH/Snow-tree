990a7f2f472a898be0b3acd1df665184c24b53eb
Jahir <93404854+JahirWH@users.noreply.github.com>
2025-02-20 11:41:22 -0600

    update
    enw

--- snow.js
+++ snow.js
@@ -16,3 +16,52 @@ function createSnowflake() {
         }
 
         setInterval(createSnowflake, 200);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const esquiador = document.querySelector(".borde-contenedor img");
+    const contenedor = document.querySelector(".borde-contenedor");
+    
+    let posicionX = 45; // Posición inicial en X (horizontal)
+    let posicionY = 140; // Posición inicial en Y (vertical)
+
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "ArrowLeft" && posicionX > 5) {
+            posicionX -= 4; // Mueve a la izquierda
+        } else if (event.key === "ArrowRight" && posicionX < 85) {
+            posicionX += 4; // Mueve a la derecha
+        } else if (event.key === "ArrowDown" && posicionY <  50) {
+            posicionY -= 10; // Mueve hacia abajo
+        }
+
+        esquiador.style.left = posicionX + "%";
+        esquiador.style.bottom = posicionY + "px"; // Mueve en el eje Y
+    });
+});
+
+
+
+//iniciar juego
+function start(){
+    i = document.getElementById('iniciar')
+    i.style.display = "none";
+}
+
+//creador de arbolito
+function arbolito() {
+            const arbol = document.createElement("div");
+            arbol.innerHTML = ;
+            snowflake.classList.add("snowflake");
+
+            snowflake.style.left = Math.random() * 100 + "vw";
+            snowflake.style.animationDuration = (Math.random() * 3 + 2) + "s"; // Entre 2 y 5s
+            snowflake.style.opacity = Math.random();
+            snowflake.style.fontSize = (Math.random() * 10 + 10) + "px"; // Entre 10px y 20px
+
+            document.body.appendChild(snowflake);
+
+            setTimeout(() => {
+                snowflake.remove();
+            }, 5000);
+        }
+
+        setInterval(createSnowflake, 200);
